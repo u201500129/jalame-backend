3c11947a0579aadedb9d38f8f6a5dde0be4440fa
Ronald Padilla Valerio <rpadilla@tarrillobarba.com.pe>
2018-07-10 18:46:06 -0500

    Bean VehiculoFoto Base64

---
+++ src/pe/tp1/hdpeta/jalame/bean/VehiculoFotoBean.java
@@ -0,0 +1,63 @@
+package pe.tp1.hdpeta.jalame.bean;
+
+import java.io.InputStream;
+
+public class VehiculoFotoBean {
+	private int codVehiculo;
+    private int codPersona;
+    private String visible;
+    private String estadoR;
+    private String foto;
+    
+       
+	public VehiculoFotoBean() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+ 
+	public VehiculoFotoBean(int codVehiculo, int codPersona, String visible, String estadoR, String foto) {
+		super();
+		this.codVehiculo = codVehiculo;
+		this.codPersona = codPersona;
+		this.visible = visible;
+		this.estadoR = estadoR;
+		this.foto = foto;
+	}
+	
+	
+	public int getCodVehiculo() {
+		return codVehiculo;
+	}
+	public void setCodVehiculo(int codVehiculo) {
+		this.codVehiculo = codVehiculo;
+	}
+	public int getCodPersona() {
+		return codPersona;
+	}
+	public void setCodPersona(int codPersona) {
+		this.codPersona = codPersona;
+	}
+	public String getVisible() {
+		return visible;
+	}
+	public void setVisible(String visible) {
+		this.visible = visible;
+	}
+	public String getEstadoR() {
+		return estadoR;
+	}
+	public void setEstadoR(String estadoR) {
+		this.estadoR = estadoR;
+	}
+	public String getFoto() {
+		return foto;
+	}
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+    
+    
+    
+    
+}
